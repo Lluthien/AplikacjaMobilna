17bb91d0b77865f600b0ee8c8aea86ababd71859
Arkadiusz Filipowski <arek0051@outlook.com>
2017-05-26 17:32:12 +0200

    Reorganizacja struktuty aplikacji i przygotowanie do integracji z serwerem - w celu przeprowadzenia procedury logowania

--- app/src/main/java/com/example/aleksandrasalak/aplikacjamobilna/MainActivity.java
+++ app/src/main/java/com/example/aleksandrasalak/aplikacjamobilna/MainActivity.java
@@ -1,33 +1,88 @@
 package com.example.aleksandrasalak.aplikacjamobilna;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
+    static final String TOKEN = "com.example.arek.TOKEN";
+    Intent tablicaActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        sharedPref = getSharedPreferences("DANE", Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
+        String ustawionyToken = sharedPref.getString(TOKEN, "Brak");
+
+        if((!ustawionyToken.equals("Brak"))){
+            // AUTORYZACJA KODU Z WYKORZYSTANIEM SERWERA
+            // JEZELI SIE POWIEDZIE
+            tablicaActivity = new Intent(this,TablicaActivity.class);
+            startActivity(tablicaActivity);
+
+            // JEZELI SIE NIE POWIEDZIE - setContentView(R.layout.main_jesli_nie_zalogowany);
+
+        }else{
+            setContentView(R.layout.main_jesli_nie_zalogowany);
+        }
     }
 
 
-    public void click (View view) {
-        Intent intent;
+    public void obslugaZdarzenAktywnosciGlownej(View view) {
+        String login = ((EditText) findViewById(R.id.loginEdit)).getText().toString();
+        String hasloPodane1 = ((EditText) findViewById(R.id.hasloEdit)).getText().toString();
 
         switch (view.getId()) {
+            case R.id.zalogujBtn:
+                // SPRAWDZENIE CZY NA SERWERZE HASLO I LOGIN SIE ZGADZAJA I POBRANIE TOKENA
+                editor.putString(TOKEN, "TOKEN_POBRANY_Z_SERWERA");
+                editor.apply();
 
-            case R.id.button:
-                intent = new Intent(MainActivity.this, LogowanieActivity.class);
-                startActivity(intent);
+                tablicaActivity = new Intent(this,TablicaActivity.class);
+                startActivity(tablicaActivity);
+
                 break;
-            case R.id.button2:
-                intent = new Intent(MainActivity.this, RejestracjaActivity.class);
-                startActivity(intent);
+            case R.id.zarejestrujBtn:
+                String hasloPodane2 = ((EditText) findViewById(R.id.haslo2Edit)).getText().toString();
+
+                if(hasloPodane1.equals(hasloPodane2)) {
+                    boolean loginPoprawny = login.matches("^.*[^a-zA-Z0-9 ].*$");
+                    if(loginPoprawny){
+                        // REJESTRACJA KONTA NA SERWERZE
+                        // O ILE SIE POWIEDZIE POBRANY JEST TOKEN I WCHODZIMY DO APLIKACJI
+                        editor.putString(TOKEN, "TOKEN_POBRANY_Z_SERWERA");
+                        editor.apply();
+
+                        tablicaActivity = new Intent(this,TablicaActivity.class);
+                        startActivity(tablicaActivity);
+                    }else{
+                        okienkoDialogowe("Podany login jest nie poprawny","Login powinien skladac sie ze znakow alfanumerycznych");
+                    }
+                }else{
+                    okienkoDialogowe("Podano dwa rozne hasla","Jako haslo wprowadz dwa identyczne ciagi znakow");
+                }
                 break;
         }
-
     }
+    public void okienkoDialogowe(String tytul,String wiadomosc){
+        new AlertDialog.Builder(this)
+                .setTitle(tytul)
+                .setMessage(wiadomosc)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
 }
---
+++ app/src/main/java/com/example/aleksandrasalak/aplikacjamobilna/TablicaActivity.java
@@ -0,0 +1,30 @@
+package com.example.aleksandrasalak.aplikacjamobilna;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TablicaActivity extends AppCompatActivity {
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
+    static final String TOKEN = "com.example.arek.TOKEN";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tablica);
+        sharedPref = getSharedPreferences("DANE", Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
+
+    }
+
+    public void wyloguj(View view){
+        editor.putString(TOKEN, "Brak");
+        editor.apply();
+        startActivity(new Intent(this,TablicaActivity.class));
+    }
+
+}
